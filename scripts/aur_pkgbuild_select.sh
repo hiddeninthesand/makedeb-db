c7804f0656aad0c2a165db33ed6c7e6684cbeae7
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-05-16 07:15:43 -0500

    Fixed syntax issue in aur_pkgbuild_select.sh

--- scripts/aur_pkgbuild_select.sh
+++ scripts/aur_pkgbuild_select.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-  rm aur_makedeb-db/PKGBUILD
-  cp makedeb-db/src/PKGBUILDs/PKGBUILD_AUR aur_makedeb-db/PKGBUILD
-
-fi
+rm aur_makedeb-db/PKGBUILD
+cp makedeb-db/src/PKGBUILDs/PKGBUILD_AUR aur_makedeb-db/PKGBUILD
